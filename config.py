745053a3e326a3e9d4b5b11e48abe3b1f7102950
Cameroonn <tyhyrhe56y@outlook.com>
2024-08-13 00:30:23 +0400

    config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+bot_token = ''
+admin_ids = []
+
+"""CRYSTALPAY"""
+auth_login = ''
+secret_1 = ''
+secret_2 = ''
+
+"""CONFIG"""
+price = 120
+worker_percent = 30
+
+"""LOLZ"""
+lolz_token = ''
+user_id = ''
+secret_answer = ''
