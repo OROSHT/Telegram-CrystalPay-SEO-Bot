a8cef9781c39dce0dc702ae2e11e7f73cf70aa26
Cameroonn <tyhyrhe56y@outlook.com>
2024-08-13 00:30:22 +0400

    bot.py

---
+++ bot.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, Dispatcher
+from config import bot_token
+from database import Database
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+bot = Bot(token=bot_token, parse_mode='HTML')
+dp = Dispatcher(bot, storage=MemoryStorage())
+db = Database('database.db')
