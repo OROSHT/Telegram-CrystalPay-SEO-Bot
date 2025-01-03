319d20b15e1fb5785aa64c76752a7b1735496734
LeandsT <leandroerhard@gmail.com>
2025-01-03 23:29:18 +0300

    bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot, Dispatcher
-from config import bot_token
-from database import Database
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-bot = Bot(token=bot_token, parse_mode='HTML')
-dp = Dispatcher(bot, storage=MemoryStorage())
-db = Database('database.db')
